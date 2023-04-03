f00041910d8e0987d9db9e732501f50b2ac0fabf
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2023-04-03 19:37:10 +0300

    весь адаптив и JS на открытие и закрытие попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,32 @@
 const buttonOpenPopup = document.querySelector(
   ".content__button-tasks_type_tasks"
 )
-console.log()
 
 const buttonClosePopup = document.querySelector(".popup__close")
-console.log()
 
 const popup = document.querySelector(".popup")
-console.log()
 
 function openPopup(popup) {
   popup.classList.add("popup_opened")
-  console.log("gggggggggggg")
+  document.addEventListener("keydown", closePopupOnEsc)
 }
 
 function closePopup(popup) {
   popup.classList.remove("popup_opened")
-  console.log()
+  document.removeEventListener("keydown", closePopupOnEsc)
 }
+
+function closePopupOnEsc(event) {
+  if (event.code == "Escape") {
+    closePopup(popup)
+  }
+}
+
+popup.addEventListener("click", (evt) => {
+  if (evt.currentTarget === evt.target) {
+    closePopup(popup)
+  }
+})
 
 buttonOpenPopup.addEventListener("click", () => {
   openPopup(popup)
@@ -27,6 +36,7 @@ buttonClosePopup.addEventListener("click", () => {
   closePopup(popup)
 })
 
+//КНОПКИ
 const buttonContentInput2Status = document.querySelector(
   ".content__button-input2_type_status"
 )
@@ -34,7 +44,6 @@ const buttonContentInput2Status = document.querySelector(
 const buttonContentInput2Verification = document.querySelector(
   ".content__button-input2_type_verification"
 )
-console.log()
 
 const buttonContentInput2Task1 = document.querySelector(
   ".content__button-input2_type_task1"
@@ -48,6 +57,7 @@ const buttonContentInput2Task3 = document.querySelector(
   ".content__button-input2_type_task3"
 )
 
+//ПОПАПЫ
 const popupContentStatus = document.querySelector(
   ".content__form2-popup_type_status"
 )
@@ -68,13 +78,130 @@ const popupContentTask3 = document.querySelector(
   ".content__form2-popup_type_task3"
 )
 
-console.log(buttonContentInput2Task1)
+//ФУНКЦИИ
 
 function openPopupContent(popup) {
+  console.log("функция открытия")
   popup.classList.add("content__form2-popup_opened")
-  console.log("mmmmmmmmmmmmmmmmmmmmmmmmmm")
 
+  document.addEventListener("keydown", closePopupContentOnEsc)
 }
+
+function closePopupContent(popup) {
+  popup.classList.remove("content__form2-popup_opened")
+  document.removeEventListener("keydown", closePopupContentOnEsc)
+}
+
+function closePopupContentOnEsc(event) {
+  const popup = document.querySelector(".content__form2-popup_opened")
+  if (event.code == "Escape") {
+    closePopupContent(popup)
+  }
+}
+
+//ЗАКРЫТИЕ ПОПАПОВ ПО КЛИКУ ВНЕ ПОПАПА
+
+document.addEventListener("click", ({ target: element }) => {
+  console.log(element)
+  if (
+    element.classList.contains("content__form2-popup_type_verification") ||
+    element.classList.contains("content__button-input2_type_verification")
+  )
+    return
+
+  let popupOpened = document.querySelector(
+    `${".content__form2-popup_type_verification"}.content__form2-popup_opened`
+  )
+
+  console.log(popupOpened)
+
+  if (!popupOpened) return
+
+  closePopupContent(popupOpened)
+  console.log("функция закрытия по window")
+})
+
+document.addEventListener("click", ({ target: element }) => {
+  console.log(element)
+  if (
+    element.classList.contains("content__form2-popup_type_task3") ||
+    element.classList.contains("content__button-input2_type_task3")
+  )
+    return
+
+  let popupOpened = document.querySelector(
+    `${".content__form2-popup_type_task3"}.content__form2-popup_opened`
+  )
+
+  console.log(popupOpened)
+
+  if (!popupOpened) return
+
+  closePopupContent(popupOpened)
+  console.log("функция закрытия по window")
+})
+
+document.addEventListener("click", ({ target: element }) => {
+  console.log(element)
+  if (
+    element.classList.contains("content__form2-popup_type_task2") ||
+    element.classList.contains("content__button-input2_type_task2")
+  )
+    return
+
+  let popupOpened = document.querySelector(
+    `${".content__form2-popup_type_task2"}.content__form2-popup_opened`
+  )
+
+  console.log(popupOpened)
+
+  if (!popupOpened) return
+
+  closePopupContent(popupOpened)
+  console.log("функция закрытия по window")
+})
+
+document.addEventListener("click", ({ target: element }) => {
+  console.log(element)
+  if (
+    element.classList.contains("content__form2-popup_type_task1") ||
+    element.classList.contains("content__button-input2_type_task1")
+  )
+    return
+
+  let popupOpened = document.querySelector(
+    `${".content__form2-popup_type_task1"}.content__form2-popup_opened`
+  )
+
+  console.log(popupOpened)
+
+  if (!popupOpened) return
+
+  closePopupContent(popupOpened)
+  console.log("функция закрытия по window")
+})
+
+document.addEventListener("click", ({ target: element }) => {
+  console.log(element)
+  if (
+    element.classList.contains("content__form2-popup_type_status") ||
+    element.classList.contains("content__button-input2_type_status")
+  )
+    return
+
+  let popupOpened = document.querySelector(
+    `${".content__form2-popup_type_status"}.content__form2-popup_opened`
+  )
+
+  console.log(popupOpened)
+
+  if (!popupOpened) return
+
+  closePopupContent(popupOpened)
+  console.log("функция закрытия по window")
+})
+
+//ОБРАБОТЧИКИ НА ОТКРЫТИЕ ПОПАПОВ
 
 buttonContentInput2Status.addEventListener("click", () => {
   openPopupContent(popupContentStatus)
@@ -95,3 +222,27 @@ buttonContentInput2Task2.addEventListener("click", () => {
 buttonContentInput2Task3.addEventListener("click", () => {
   openPopupContent(popupContentTask3)
 })
+
+//ОБРАБОТЧИКИ НА ЗАКРЫТИЕ ПОПАПОВ
+
+popupContentStatus.addEventListener("click", () => {
+  closePopupContent(popupContentStatus)
+})
+
+popupContentVerification.addEventListener("click", () => {
+  closePopupContent(popupContentVerification)
+})
+
+popupContentTask1.addEventListener("click", () => {
+  closePopupContent(popupContentTask1)
+})
+
+popupContentTask2.addEventListener("click", () => {
+  closePopupContent(popupContentTask2)
+})
+
+popupContentTask3.addEventListener("click", () => {
+  closePopupContent(popupContentTask3)
+})
+
+console.log()
--- scripts/specific.js
+++ scripts/specific.js
@@ -1,13 +1,42 @@
 function openPopupContent(popup) {
   popup.classList.add("content__form2-popup_opened")
-  console.log("fffffffffffff")
+  document.addEventListener("keydown", closePopupContentOnEsc)
 }
+
+function closePopupContent(popup) {
+  popup.classList.remove("content__form2-popup_opened")
+  document.removeEventListener("keydown", closePopupContentOnEsc)
+}
+
+function closePopupContentOnEsc(event) {
+  if (event.code == "Escape") {
+    closePopupContent(popupContentSpecific)
+  }
+}
+
+document.addEventListener("click", ({ target: element }) => {
+  console.log(element)
+  if (
+    element.classList.contains("content__form2-popup_type_specific") ||
+    element.classList.contains("content__button-input2_type_specific")
+  )
+    return
+
+  let popupOpened = document.querySelector(
+    `${".content__form2-popup_type_specific"}.content__form2-popup_opened`
+  )
+
+  console.log(popupOpened)
+
+  if (!popupOpened) return
+
+  closePopupContent(popupOpened)
+  console.log("функция закрытия по window")
+})
 
 const buttonContentInput2Specific = document.querySelector(
   ".content__button-input2_type_specific"
 )
-
-console.log(buttonContentInput2Specific)
 
 const popupContentSpecific = document.querySelector(
   ".content__form2-popup_type_specific"
@@ -15,6 +44,10 @@ const popupContentSpecific = document.querySelector(
 
 buttonContentInput2Specific.addEventListener("click", () => {
   openPopupContent(popupContentSpecific)
+})
+
+popupContentSpecific.addEventListener("click", () => {
+  closePopupContent(popupContentSpecific)
 })
 
 console.log()
